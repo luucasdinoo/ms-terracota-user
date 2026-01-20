fa3ed76374b5746963d6b7f076bfd4ba1cb7fbb7
Lucas Bernadino Silvestre da Silva <luucasdinoo@gmail.com>
2026-01-20 20:20:16 -0300

    config(terracota): Initial Commit

---
+++ src/main/java/br/com/terracota/MsTerracotaUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.terracota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTerracotaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTerracotaUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/terracota/MsTerracotaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.terracota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTerracotaUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
